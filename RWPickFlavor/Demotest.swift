edbf17c541a76a5ae5402427a3afa5e81a3d95f9
NGUYEN MINH THANH <tha_nguyen@vision-net.co.jp>
2020-01-07 11:46:08 +0700

    Add existing file

---
+++ RWPickFlavor/Demotest.swift
@@ -0,0 +1,13 @@
+//
+//  Demotest.swift
+//  RWPickFlavor
+//
+//  Created by NGUYEN MINH THANH on 1/7/20.
+//  Copyright © 2020 NGUYEN MINH THANH. All rights reserved.
+//
+
+import UIKit
+
+class Demotest: NSObject {
+
+}
